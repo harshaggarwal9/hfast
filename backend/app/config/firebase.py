87c40219d6283d9d30c58a9511d9972a28f2d57d
harshaggarwal9 <harshaggarwal935@gmail.com>
2026-01-14 21:11:24 +0530

    Fix Firebase init for Render using env credentials

--- backend/app/config/firebase.py
+++ backend/app/config/firebase.py
@@ -1,8 +1,10 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_key.json")
-
+        cred_json = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+        cred = credentials.Certificate(cred_json)
         firebase_admin.initialize_app(cred)
